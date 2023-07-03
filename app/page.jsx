0cf89a430587b67d927017a0ca98d11bc99a4f36
mohammad_rizwan <rizwanm76@yahoo.com>
2023-07-03 14:48:02 -0400

    added Home Component and styling with tailwind

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,14 @@ const Home = () => {
         <section className="w-full flex-center flex-col">
             <h1 className="head_text text-center">
                 Discover & Share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center"> AI-Powered Prompts </span>
             </h1>
+            <p className="desc text-center">
+                RizzyPrompts is an open-source AI prompting tool for the modern world to discover, create and share creative prompts
+            </p>
+
+            {/* Feed Component */}
         </section>
     )
 }
