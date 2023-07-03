97ad9f25043257a66c67d3489f24c5de61ee217b
mohammad_rizwan <rizwanm76@yahoo.com>
2023-07-03 15:32:21 -0400

    added navbar with desktop view

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import "@styles/globals.css";
 
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
 export const metadata = {
     title: "RizzyPrompts",
     description: 'Discover, Share, and Create AI Prompts'
@@ -15,6 +18,7 @@ const RootLayout = ({ children }) => {
                 </div>
 
                 <main className='app'>
+                    <Nav />
                     {children}
                 </main>
             </body>
