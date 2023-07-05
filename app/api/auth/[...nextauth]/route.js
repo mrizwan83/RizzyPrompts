ce492dfaae62cb3255214df8e25355782cc3b324
mohammad_rizwan <rizwanm76@yahoo.com>
2023-07-04 20:01:03 -0400

    setup backend connection to mongodb

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,22 +1,37 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 
+import { connectToDB } from "@utils/database";
+
 // boiler plate code from Nextjs docs 
 // check out for more info
 // https://next-auth.js.org/configuration/initialization#route-handlers-app
+
 
 const handler = NextAuth({
     providers: [
         // OAuth authentication providers...
         GoogleProvider({
-            clientId: '',
-            clientSecret: ''
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
     async session({ session }) {
 
     },
+    // serverless route -> lambda function -> only spins up when called ->
     async signIn({ profile }) {
+        try {
+            await connectToDB();
+            // check if a user already exists
+
+            // if not, create a new user
+
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
 
     }
 });
