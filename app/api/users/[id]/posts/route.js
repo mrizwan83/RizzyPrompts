d54a83e6c41aa9e4d0d03a00ddfabd80e74d4052
mohammad_rizwan <rizwanm76@yahoo.com>
2023-07-11 03:04:59 -0400

    add dynamic backend route for a specific user's prompts

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,14 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+export const GET = async (req, { params }) => {
+    try {
+        await connectToDB();
+        // get all prompts populate the creator from the db
+        const prompts = await Prompt.find({
+            creator: params.id
+        }).populate('creator');
+        return new Response(JSON.stringify(prompts), { status: 200 })
+    } catch (error) {
+        return new Response("Failed to fetch all prompts!", { status: 500 })
+    }
+}
