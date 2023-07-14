4e33feaaef6fd2382619a11f1c72707a8b9f4205
mohammad_rizwan <rizwanm76@yahoo.com>
2023-07-14 00:08:50 -0400

    added backend logic to sort the prompts on total votes before sending res

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,12 +1,22 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+
 export const GET = async (req) => {
     try {
         await connectToDB();
-        // get all prompts populate the creator from the db
-        const prompts = await Prompt.find({}).populate('creator');
-        return new Response(JSON.stringify(prompts), { status: 200 })
+
+        const prompts = await Prompt.find({})
+            .populate('creator')
+            .lean(); // Convert Mongoose documents to plain JavaScript objects
+
+        // Calculate voteCount for each prompt and sort them
+        prompts.forEach((prompt) => {
+            prompt.voteCount = prompt.upvotes - prompt.downvotes;
+        });
+        prompts.sort((a, b) => b.voteCount - a.voteCount);
+
+        return new Response(JSON.stringify(prompts), { status: 200 });
     } catch (error) {
-        return new Response("Failed to fetch all prompts!", { status: 500 })
+        return new Response("Failed to fetch all prompts!", { status: 500 });
     }
-}
+};
